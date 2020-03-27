c052adf42e82b7efa4daefb7433a4980740827a7
Jens Reidel <jens@troet.org>
2020-03-27 18:16:09 +0100

    new ordering tool

--- run.sh
+++ run.sh
@@ -4,4 +4,5 @@ rm index
 podman build -t wheels:latest . --no-cache
 podman run -d --rm --name wheels wheels:latest
 podman cp --pause=false wheels:/wheels/ .
-ls wheels >> index
+ls wheels > index
+python3.8 order.py > index-order
