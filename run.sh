025c614f3dd506371a6b920cac668fb3d10f9a6b
Jens Reidel <jens@troet.org>
2020-09-30 19:05:35 +0200

    Use temp orjson fix and latest nightly again

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 export PATH=$HOME/.local/bin:$PATH
 ./delete-git-hashes.sh
-rm wheels/*
 podman build -t wheels:latest . --no-cache
 podman run -d --rm --name wheels wheels:latest
 podman cp wheels:/wheels/ .
